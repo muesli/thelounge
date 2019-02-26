f16f3268b7668b315e1c38a1ddebcace69c1b985
Richard Lewis <richrd.lewis@gmail.com>
2019-02-26 22:23:41 +0200

    Add vuex and move isConnected to vuex state.

--- client/js/upload.js
+++ client/js/upload.js
@@ -93,7 +93,7 @@ class Uploader {
 			return;
 		}
 
-		if (!this.vueApp.isConnected) {
+		if (!this.vueApp.$store.state.isConnected) {
 			this.handleResponse({
 				error: `You are currently disconnected, unable to initiate upload process.`,
 			});
@@ -181,9 +181,7 @@ class Uploader {
 		this.setProgress(0);
 
 		if (response.error) {
-			// require here due to circular dependency
-			const {vueApp} = require("./vue");
-			vueApp.currentUserVisibleError = response.error;
+			this.vueApp.currentUserVisibleError = response.error;
 			return;
 		}
 
